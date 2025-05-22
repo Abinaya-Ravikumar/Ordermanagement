a466bceccf322f51a053394638b514a706aacab1
Abinaya R <126380093+Abinaya-Ravikumar@users.noreply.github.com>
2025-05-22 12:10:25 +0530

    Create appdb.sql

---
+++ appdb.sql
@@ -0,0 +1,7 @@
+CREATE TABLE orders (
+    orderId INT AUTO_INCREMENT PRIMARY KEY,
+    customerName VARCHAR(100) NOT NULL,
+    orderDate DATE NOT NULL,
+    totalAmount DECIMAL(10,2),
+    orderStatus VARCHAR(50) NOT NULL
+);
